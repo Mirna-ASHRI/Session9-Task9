03f5920a88471f5820f9d826a85672445e4e3e76
Mirna-ASHRI <168423587+Mirna-ASHRI@users.noreply.github.com>
2024-05-31 20:54:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// window.alert("This is the alert");
+// document.getElementById("hello").style.color = "blue";
+// console.log("Hello World")
+
+// let name = "Ahmed Mohamed Ismael";
+// let age = 35;
+
+// console.log(`My name is:${name} and my age is: ${age}`)
+
+// TASK1
+ var x = ("I want")
+ var y = ("to be")
+ var z = ("a Software Developer")
+ console.log(x + " " + y + " " + z)
+
+ var m = " "
+
+ let text = x + m + y + m + z
+ document.write(text)
+
+// TASK2
+
+var firstName = ["Mirna", "Mai", "Mariam", "Maha"]
+var age = ["24", "26", "28", "30"]
+var jobTitle = ["Pharmacist", "Physician", "Data Analyst", "Software Developer"]
+
+for (let i = 0; i < firstName.length; i++)
+console.log("I'm" + m + firstName[i] + m + "and my age is" + m + age[i] + m + "and i am working as a" + m + jobTitle[i])
